0b93db059a7bb749806b99f1adf40d6a382650c8
SHRIRAM7V <230121.IT@rmkec.ac.in>
2025-06-22 14:27:48 +0530

    Load assets,constants

---
+++ components/home/popular/Popularjobs.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
+import { useState } from "react";
+import styles from "./popularjobs.style";
+import { useRouter } from "expo-router";
+import { SIZES, COLORS } from "../../../constants";
+import PopularJobCard from "../../common/cards/popular/PopularJobCard";
+
+const Popularjobs = () => {
+  const router = useRouter();
+
+  const [isLoading, setisLoading] = useState(false);
+  const [error, seterror] = useState(false);
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size="large" colors={COLORS.primary} />
+        ) : error ? (
+          <Text>Something went Wrong</Text>
+        ) : (
+          <FlatList
+            data={[1, 2, 3, 4, 5, 6, 7, 8]}
+            renderItem={({ item }) => <PopularJobCard item={item} />}
+            keyExtractor={(item) => item?.job_id}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
+            horizontal
+          />
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default Popularjobs;
---
+++ constants/images.js
@@ -0,0 +1,6 @@
+import profile from "../assets/images/kemal.jpg";
+import profile2 from "../assets/profile.jpeg";
+export default {
+  profile,
+  profile2,
+};
